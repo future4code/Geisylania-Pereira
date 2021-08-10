70b4197b6ecc8b945d6b6115e5c5b3392cb86753
GeisyLopes <geisylopes7l@gmail.com>
2021-08-10 16:36:34 -0300

    adicinando exercicios

---
+++ semana1/operadores/index.js
@@ -0,0 +1,38 @@
+
+// false
+
+//false
+
+//true
+
+// boolean
+
+
+
+// prompt é uma pergunta, ex "Qual seu nome?"
+// para o usuario responder,no caso ai
+
+
+//Exercicio 1
+
+//const minhaIdade = prompt("Qual sua idade? ")
+//const amigo = prompt("Qual é sua idade? ")
+
+
+//console.log("Sua idade é menor do que a do seu amigo? ", minhaIdade < amigo )
+// 26 < 30 // true, porque o 26 é menor que 30
+
+//let diferença = minhaIdade - amigo
+
+//console.log("resultado subtraçao ", minhaIdade - amigo)
+
+//resultado = minhaIdade - amigo
+
+
+// Exercicio 2
+
+//const numeroPar = prompt("insira o numero par: ")
+
+//console.log("resultado par: ", numeroPar / 2 )
+
+// dividiria por 2 do mesmo jeito, nao daria erro 
