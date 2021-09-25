62ee97ef4499b7e999a3a82c7d4215c3f8ffbff1
GeisyLopes <geisylopes7l@gmail.com>
2021-09-25 11:55:34 -0300

    integracao

---
+++ semana7/labefyplay/my-play/my-play1/src/componets/header/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled"
+
+const HeaderContainer = styled.div`
+    height: 100px;
+    display: flex;
+`
+const Header = () => {
+    return (
+        <div>
+        Header
+        </div>
+    )
+}
+
+export default Header
