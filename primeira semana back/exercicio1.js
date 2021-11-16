381e3f1daa369e5501d7f3ed84a9daeaee4ed2a1
GeisyLopes <geisylopes7l@gmail.com>
2021-11-16 16:53:44 -0300

    exercicios back

---
+++ primeira semana back/exercicio1.js
@@ -0,0 +1,10 @@
+//Exercicio 1 
+//Questões
+// a) Responda como comentário no seu código: como fazemos para acessar os parâmetros passados na linha de comando para o Node?
+// b) Crie um programa que receba seu nome e sua idade. Após receber estes valores, imprima no console uma mensagem que siga a seguinte estrutura:
+// c) Altere o programa acima para que mostre também a sua idade daqui a sete anos.
+
+const nome = "Geisy"
+const idade = 26
+const daquiSeteAnos = (idade + 7)
+console.log(`Ola, ${nome}! Você tem ${idade} anos. Em sete anos você tera ${daquiSeteAnos}`)
---
+++ primeira semana back/exercicio2.js
@@ -0,0 +1,7 @@
+console.log("Exercicio 2")
+const operacao = process.argv[2];
+const 
+
+const resultados = () => {
+    switch
+}
